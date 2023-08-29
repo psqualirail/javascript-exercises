da57fa1769bf401c1119953ec6ac1d4d0558a6f1
Pasquale Carpino <142628150+psqualirail@users.noreply.github.com>
2023-08-29 13:11:02 -0400

    feat: complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if ((typeof a !== 'number') || (typeof b !== 'number')) {
+        return 'ERROR'
+    } else if ((a < 0) || (b < 0)) {
+        return 'ERROR'
+    }
+    else if (a > b) {
+        let c = a;
+        a = b;
+        b = c;
+    }
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
