4b5c6ef21744f3cb67f2c26ebc6843f8bf57673c
Pasquale Carpino <142628150+psqualirail@users.noreply.github.com>
2023-08-29 11:19:14 -0400

    feat: complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let arr = [];
+    for (let i = s.length-1; i >= 0; i--) {
+        arr.push(s[i]);
+    }
+    return arr.join('');
 };
 
 // Do not edit below this line
