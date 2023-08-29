9058295865a00d1923a2401f01e522c46da55094
Pasquale Carpino <142628150+psqualirail@users.noreply.github.com>
2023-08-29 13:20:03 -0400

    feat: complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let divBy400 = true;
+    if (year % 100 == 0) {
+        if (year % 400 != 0) divBy400 = false;
+    }
+    return (year % 4 == 0 && divBy400); 
 };
 
 // Do not edit below this line
