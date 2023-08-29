2f34162b02003b6e3b398764ef81b3c2669a58c5
Pasquale Carpino <142628150+psqualirail@users.noreply.github.com>
2023-08-29 13:04:17 -0400

    feat: complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a, ...toBeRemoved) {
+    for (const toRemove of toBeRemoved) {
+        a = a.filter(element => element !== toRemove)
+    }
+    return a;
 };
 
 // Do not edit below this line
