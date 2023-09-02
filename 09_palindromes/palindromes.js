f63d562f4d8e88e7cd2e9fd0b368547c7b50385e
Pasquale Carpino <142628150+psqualirail@users.noreply.github.com>
2023-09-02 14:53:28 -0400

    feat: complete exercise 9, palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    const cleanString = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return cleanString.split("").reverse().join("") == cleanString;
 };
 
 // Do not edit below this line
