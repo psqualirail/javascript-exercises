ac4b6dfeb6e63778bc1901e2af6fa18a54b3137f
Pasquale Carpino <142628150+psqualirail@users.noreply.github.com>
2023-09-02 14:33:01 -0400

    feat: complete exercise 8 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let arg of array) {
+    sum += arg;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (let arg of array) {
+    result *= arg;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a < 0) {
+    return undefined;
+  }
+  let result = 1;
+  while (a > 1) {
+    result *= a;
+    a--; 
+  }
+  return result;
 };
 
 // Do not edit below this line
