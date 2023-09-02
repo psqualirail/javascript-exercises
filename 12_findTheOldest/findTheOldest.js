144bd38b2c0c2803a411368e172b8c2f62772171
Pasquale Carpino <142628150+psqualirail@users.noreply.github.com>
2023-09-02 16:11:59 -0400

    feat: complete exercise 12, findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort(function(a, b) {
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        if (aAge > bAge) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return array[0];
 };
 
 // Do not edit below this line
