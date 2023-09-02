fda2bfa79531328c21a113e9aee9d48fc7f1f8af
Pasquale Carpino <142628150+psqualirail@users.noreply.github.com>
2023-09-02 15:01:55 -0400

    feat: complete exercise 10, fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const array = [1, 1];
+    if (n <= 0) return "OOPS";
+    if (n <= 2) return array[n-1];
+    for (let i=3; i<=n; i++) {
+        array.push(array[i-3] + array[i-2]);
+    }
+    return array[n-1];
 };
 
 // Do not edit below this line
